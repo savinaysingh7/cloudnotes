76eaf1689003913597814c0761eec96dfc429bda
savinaysingh7 <savinay07singh@gmail.com>
2026-05-22 14:15:53 +0530

    Migrate local Jenkins setup to JCasC (Configuration as Code)

--- jenkins/init.groovy.d/security.groovy
+++
@@ -1,9 +0,0 @@
-import jenkins.model.*
-import hudson.security.*
-def instance = Jenkins.getInstance()
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin")
-instance.setSecurityRealm(hudsonRealm)
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-instance.setAuthorizationStrategy(strategy)
-instance.save()
